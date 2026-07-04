7509ff658b89a59c12e4044c042b27cf2ef7da4c
aditighoshagd <aditighoshagd@gmail.com>
2026-07-04 17:09:11 +0530

    feat: implement post service APIs and authentication

--- postsService/src/main/java/com/loom/postsService/PostServiceApplication.java
+++ postsService/src/main/java/com/loom/postsService/PostServiceApplication.java
@@ -1,4 +1,4 @@
-package com.loom.postService;
+package com.loom.postsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
