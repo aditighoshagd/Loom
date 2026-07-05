5200ab853718701d1e76ed63c48b6af24613473d
aditighoshagd <aditighoshagd@gmail.com>
2026-07-06 02:03:23 +0530

    ConnectionsService added

---
+++ ConnectionsService/src/main/java/com/loom/ConnectionsService/ConnectionsServiceApplication.java
@@ -0,0 +1,13 @@
+package com.loom.ConnectionsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConnectionsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConnectionsServiceApplication.class, args);
+	}
+
+}
--- postsService/src/main/java/com/loom/postsService/PostServiceApplication.java
+++ postsService/src/main/java/com/loom/postsService/PostServiceApplication.java
@@ -2,8 +2,10 @@ package com.loom.postsService;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class PostServiceApplication {
 
 	public static void main(String[] args) {
