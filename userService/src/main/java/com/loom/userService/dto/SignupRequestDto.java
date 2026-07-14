8da87cae7be03ac1ebbf5f25fb893320f02c6048
aditighoshagd <aditighoshagd@gmail.com>
2026-07-14 23:36:25 +0530

    Update user database schema and data transfer models with profile picture

--- userService/src/main/java/com/loom/userService/dto/SignupRequestDto.java
+++ userService/src/main/java/com/loom/userService/dto/SignupRequestDto.java
@@ -4,5 +4,8 @@ import lombok.Data;
 
 @Data
 public class SignupRequestDto {
-    private String name, email, password;
+    private String name;
+    private String email;
+    private String password;
+    private String profilePictureUrl;
 }
--- userService/src/main/java/com/loom/userService/dto/UserDto.java
+++ userService/src/main/java/com/loom/userService/dto/UserDto.java
@@ -5,5 +5,7 @@ import lombok.Data;
 @Data
 public class UserDto {
     private Long id;
-    private String name, email;
+    private String name;
+    private String email;
+    private String profilePictureUrl;
 }
