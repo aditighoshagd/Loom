e61b16cae482298436eb67981b54e0709704f1b7
aditighoshagd <aditighoshagd@gmail.com>
2026-07-14 23:37:03 +0530

    Implement user core controller for profile picture modifications

---
+++ userService/src/main/java/com/loom/userService/controller/UserCoreController.java
@@ -0,0 +1,23 @@
+package com.loom.userService.controller;
+
+import com.loom.userService.dto.UserDto;
+import com.loom.userService.service.AuthService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/core")
+@RequiredArgsConstructor
+public class UserCoreController {
+
+    private final AuthService authService;
+
+    @PutMapping("/profile-picture")
+    public ResponseEntity<UserDto> updateProfilePicture(
+            @RequestHeader("X-User-Id") Long userId,
+            @RequestBody String profilePictureUrl) {
+        UserDto userDto = authService.updateProfilePicture(userId, profilePictureUrl);
+        return ResponseEntity.ok(userDto);
+    }
+}
