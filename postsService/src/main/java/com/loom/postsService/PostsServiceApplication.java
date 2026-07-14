b0f0f37b3eda8a7ffec5c0032b5676dd81a2b5d3
aditighoshagd <aditighoshagd@gmail.com>
2026-07-14 22:13:52 +0530

    Refactor posts service main application class and test files

--- postsService/src/main/java/com/loom/postsService/PostsServiceApplication.java
+++ postsService/src/main/java/com/loom/postsService/PostsServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-public class PostServiceApplication {
+public class PostsServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(PostServiceApplication.class, args);
+		SpringApplication.run(PostsServiceApplication.class, args);
 	}
 
 }
