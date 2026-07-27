7954d5d7c030644ea8cd86db2e1ba6733c61fe75
aditighoshagd <aditighoshagd@gmail.com>
2026-07-27 05:49:04 +0530

    Add logging to authentication endpoints in user service

--- userService/src/main/java/com/loom/userService/controller/UserController.java
+++ userService/src/main/java/com/loom/userService/controller/UserController.java
@@ -5,6 +5,7 @@ import com.loom.userService.dto.SignupRequestDto;
 import com.loom.userService.dto.UserDto;
 import com.loom.userService.service.AuthService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/auth")
 @RequiredArgsConstructor
@@ -21,12 +23,14 @@ public class UserController {
 
     @PostMapping("/signup")
     public ResponseEntity<UserDto> signUp(@RequestBody SignupRequestDto signupRequestDto) {
+        log.info("Processing user signup request for email: {}", signupRequestDto.getEmail());
         UserDto userDto = authService.signUp(signupRequestDto);
         return new ResponseEntity<>(userDto, HttpStatus.CREATED);
     }
 
     @PostMapping("/login")
     public ResponseEntity<String> login(@RequestBody LoginRequestDto loginRequestDto) {
+        log.info("Processing user login request for email: {}", loginRequestDto.getEmail());
         String token = authService.login(loginRequestDto);
         return ResponseEntity.ok(token);
     }
